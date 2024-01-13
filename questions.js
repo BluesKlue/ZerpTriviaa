4c8b4ae2051726c05095d13aae5d8f1aa682560b
BluezKlue <130177578+BluesKlue@users.noreply.github.com>
2024-01-13 16:30:38 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        Number: 1,
+        question: "Why are jeans blue?",
+        answer: "A. The dye used was readily available and inexpensive",
+        options: [
+            "A. The dye used was readily available and inexpensive",
+            "B. Blue was the favorite color of the original denim designer",
+            "C. Blue color provides better durability to the fabric",
+            "D. The dye used was the only color available at the time",
+        ]
+    },
+    {
+        Number: 2,
+        question: "Which element has the highest melting point?",
+        answer: "C. Tungsten",
+        options: [
+            "A. Iron",
+            "B. Platinum",
+            "C. Tungsten",
+            "D. Cobalt",
+        ]
+    },
+    {
+        Number: 3,
+        question: "What is the capital city of Bhutan?",
+        answer: "D. Thimphu",
+        options: [
+            "A. Tamji",
+            "B. Tala",
+            "C. Taripe",
+            "D. Thimphu",
+        ]
+    },
+    {
+        Number: 4,
+        question: "In which year did the Titanic sink?",
+        answer: "C. 1912",
+        options: [
+            "A. 1921",
+            "B. 1905",
+            "C. 1912",
+            "D. 1918",
+        ]
+    },
+    {
+        Number: 5,
+        question: "Which element is named after the Greek word for (stone)?",
+        answer: "B. Lithium",
+        options: [
+            "A. Potassium",
+            "B. Lithium",
+            "C. Argon",
+            "D. Granite",
+        ]
+    },
+    {
+        Number: 6,
+        question: "What is the largest desert in the world by area?",
+        answer: "C. Antarctica",
+        options: [
+            "A. Sahara",
+            "B. Gobi",
+            "C. Antarctica",
+            "D. Arabian",
+        ]
+    },
+    {
+        Number: 7,
+        question: "Which country has the most time zones?",
+        answer: "D. France",
+        options: [
+            "A. Australia",
+            "B. United States",
+            "C. China",
+            "D. France",
+        ]
+    },
+    {
+        Number: 8,
+        question: "What is the only letter that does not appear in the periodic table?",
+        answer: "D. J",
+        options: [
+            "A. M",
+            "B. D",
+            "C. K",
+            "D. J",
+        ]
+    },
+    {
+        Number: 9,
+        question: "How many hearts does an octopus have?",
+        answer: "C. 3",
+        options: [
+            "A. 1",
+            "B. 2",
+            "C. 3",
+            "D. 4",
+        ]
+    },
+    {
+        Number: 10,
+        question: "What is the fear of the number 13 called?",
+        answer: "C. Triskaidekaphobia",
+        options: [
+            "A. Trypophobia",
+            "B. Scriptophobia",
+            "C. Triskaidekaphobia",
+            "D. Graphophobia",
+        ]
+    }
+]
